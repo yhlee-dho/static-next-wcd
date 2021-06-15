d0d876715af79c65369df58926b1dc6a6ede4331
Yong Lee <yong@windycitydevs.io>
2021-06-15 15:21:35 -0500

    removing problems

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,22 +7,18 @@ import Head from 'next/head';
 import Container from 'components/container';
 import Blog from 'components/blog';
 import Testimonial from 'components/testimonial';
-// import MoreCards from 'components/more-cards';
 
-import { getAllPosts } from 'lib/api';
 import { CMS_NAME } from 'lib/constants';
 import Post from 'types/post';
 
 
 type Props = {
-	allPosts: Post[];
-	props: string | number;
+	props: string|number;
 	preview?: boolean;
 };
 
-const Index = ({ allPosts, props, preview }: Props) => {
-	const morePosts = allPosts.slice(0);
-	
+const Index = ({ props, preview }: Props) => {
+
 	return (
 		<>
 			<Layout preview={preview}>
@@ -50,9 +46,7 @@ const Index = ({ allPosts, props, preview }: Props) => {
 					{/* project cards */}
 					
 
-					{/* <div className={`max-w-5x1 mt-5 mb-5 grid mx-auto content-center justify-center items-center text-center`}>
-						{morePosts.length > 0 && <MoreCards posts={morePosts} />}
-					</div> */}
+
 				</Container>
 			</Layout>
 		</>
